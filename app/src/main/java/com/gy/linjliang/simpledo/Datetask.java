ae202a95c716dfe84f8d45bbfd03c615f1724b14
Linjliang <864483116@qq.com>
2018-01-06 11:40:48 +0800

    初始的修改以便配合

---
+++ app/src/main/java/com/gy/linjliang/simpledo/Datetask.java
@@ -0,0 +1,16 @@
+package com.gy.linjliang.simpledo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenovo on 2018/1/6.
+ */
+
+public class Datetask extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datetask);
+    }
+}
